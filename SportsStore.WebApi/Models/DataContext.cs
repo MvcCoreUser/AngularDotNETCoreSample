a3322d4a37e41e1e0738d4a7d821f88eb793f785
Evgen <evgeniyap88@gmail.com>
2019-07-12 23:28:22 +0500

    add data context class

---
+++ SportsStore.WebApi/Models/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SportsStore.WebApi.Models
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+        public DbSet<Rating> Ratings { get; set; }
+    }
+}
