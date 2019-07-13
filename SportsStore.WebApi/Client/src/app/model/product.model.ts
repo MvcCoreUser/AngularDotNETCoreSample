d33b2a059aa0ea300ab113bb23f7fd6ae4424393
Evgen <evgeniyap88@gmail.com>
2019-07-13 20:42:54 +0500

    page 67, add new angular models

--- SportsStore.WebApi/Client/src/app/model/product.model.ts
+++ SportsStore.WebApi/Client/src/app/model/product.model.ts
@@ -1,9 +1,14 @@
+import { Supplier } from './supplier.model';
+import { Rating } from './rating.model';
+
 export class Product {
   constructor(
     public productId?: number,
     public name?: string,
     public category?: string,
     public price?: number,
-    public description?: string
+    public description?: string,
+    public supplier?: Supplier,
+    public ratings?: Rating[]
   ) {}
 }
