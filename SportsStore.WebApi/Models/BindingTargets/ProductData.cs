1c5502cf78af117da12d2e32c4ef7b3021656ce6
Evgen <evgeniyap88@gmail.com>
2019-07-19 03:50:28 +0500

    page 119, add databinding classes

---
+++ SportsStore.WebApi/Models/BindingTargets/ProductData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.WebApi.Models.BindingTargets
+{
+    public class ProductData
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Category { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage ="Price must be at least 1")]
+        public decimal Price { get; set; }
+
+        public long SupplierId { get; set; }
+
+        public Product Product => new Product
+        {
+            Name = this.Name,
+            Category = this.Category,
+            Description = this.Description,
+            Price = this.Price,
+            Supplier = this.SupplierId == 0 ? null : new Supplier { SupplierId = this.SupplierId }
+        };
+    }
+}
---
+++ SportsStore.WebApi/Models/BindingTargets/SupplierData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.WebApi.Models.BindingTargets
+{
+    public class SupplierData
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        [StringLength(2, MinimumLength =2)]
+        public string State { get; set; }
+
+        public Supplier Supplier => new Supplier
+        {
+            Name = this.Name,
+            City = this.City,
+            State = this.State
+        };
+    }
+}
