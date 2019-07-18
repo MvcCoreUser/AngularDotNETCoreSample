7d3af8ec4076dc6e1e4e7c1cb02cd59453906731
Evgen <evgeniyap88@gmail.com>
2019-07-19 04:15:46 +0500

    page 121, add web service for supplier entity

---
+++ SportsStore.WebApi/Controllers/SupplierValuesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.WebApi.Models;
+using SportsStore.WebApi.Models.BindingTargets;
+
+namespace SportsStore.WebApi.Controllers
+{
+    [Route("api/suppliers")]
+    [ApiController]
+    public class SupplierValuesController : ControllerBase
+    {
+        private DataContext context;
+
+        public SupplierValuesController(DataContext dataContext)
+        {
+            context = dataContext;
+        }
+
+        [HttpGet]
+        public IEnumerable<Supplier> Get()
+        {
+            return context.Suppliers;
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] SupplierData supplierData)
+        {
+            if (ModelState.IsValid)
+            {
+                Supplier supplier = supplierData.Supplier;
+                context.Add(supplier);
+                context.SaveChanges();
+                return Ok(supplier.SupplierId);
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+    }
+}
