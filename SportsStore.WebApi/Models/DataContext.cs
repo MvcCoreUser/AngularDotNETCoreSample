0301c37be7048137200ab4a7ac3d65bb414b68d3
Evgen <evgeniyap88@gmail.com>
2019-07-20 13:55:50 +0500

    page 141, change default on delete behavior for product-related entities

--- SportsStore.WebApi/Models/DataContext.cs
+++ SportsStore.WebApi/Models/DataContext.cs
@@ -16,5 +16,11 @@ namespace SportsStore.WebApi.Models
         public DbSet<Product> Products { get; set; }
         public DbSet<Supplier> Suppliers { get; set; }
         public DbSet<Rating> Ratings { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasMany<Rating>(p => p.Ratings).WithOne(r => r.Product).OnDelete(DeleteBehavior.Cascade);
+            modelBuilder.Entity<Product>().HasOne<Supplier>(p => p.Supplier).WithMany(s => s.Products).OnDelete(DeleteBehavior.SetNull);
+        }
     }
 }
