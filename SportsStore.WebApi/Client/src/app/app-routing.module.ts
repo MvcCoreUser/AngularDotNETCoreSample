ee39564e5ad1bb2189cf540ccf946f0df1118168
Evgen <evgeniyap88@gmail.com>
2019-07-23 20:46:33 +0500

    page 160, add routing config

--- SportsStore.WebApi/Client/src/app/app-routing.module.ts
+++ SportsStore.WebApi/Client/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
+import { ProductDetailComponent } from './core/structure/productDetail.component';
+import { ProductTableComponent } from './core/structure/productTable.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './core/home/home.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'table', component: ProductTableComponent, pathMatch: 'full' },
+  {path: 'detail', component: ProductDetailComponent, pathMatch: 'full'},
+  {path: '', component: ProductTableComponent}
 ];
 
 @NgModule({
