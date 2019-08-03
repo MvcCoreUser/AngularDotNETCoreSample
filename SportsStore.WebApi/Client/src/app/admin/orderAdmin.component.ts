cb2b1ce6896c69b5ee4e9aceb783d53dd83a88b7
Evgen <evgeniyap88@gmail.com>
2019-08-03 18:59:10 +0500

    page 254, administering orders

--- SportsStore.WebApi/Client/src/app/admin/orderAdmin.component.ts
+++ SportsStore.WebApi/Client/src/app/admin/orderAdmin.component.ts
@@ -1,6 +1,18 @@
 import { Component } from "@angular/core";
+import { Repository } from '../model/repository.model';
+import { Order } from '../model/order.model';
 
 @Component({
   templateUrl: 'orderAdmin.component.html'
 })
-export class OrderAdminComponent{}
+export class OrderAdminComponent{
+  constructor(private repo: Repository){}
+
+  get orders():Order[]{
+    return this.repo.orders;
+  }
+
+  markShipped(order: Order){
+    this.repo.shipOrder(order);
+  }
+}
