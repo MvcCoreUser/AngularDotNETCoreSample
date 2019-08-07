d3d5e67d29686a26735bba86f26f71d9ac5ca2ed
Evgen <evgeniyap88@gmail.com>
2019-08-07 22:02:49 +0500

    page 270, add Identity Data Context and seed class

---
+++ SportsStore.WebApi/Models/IdentityDataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SportsStore.WebApi.Models
+{
+    public class IdentityDataContext: IdentityDbContext<IdentityUser>
+    {
+        public IdentityDataContext(DbContextOptions<IdentityDataContext> options): base(options)
+        {
+
+        }
+    }
+}
---
+++ SportsStore.WebApi/Models/IdentitySeedData.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SportsStore.WebApi.Models
+{
+    public class IdentitySeedData
+    {
+        private const string adminUser = "admin";
+        private const string adminPassword = "MySecret123$";
+        private const string adminRole = "Administrator";
+
+        private static T GetAppService<T>(IApplicationBuilder app)
+        {
+            return app.ApplicationServices.GetRequiredService<T>();
+        }
+
+        public static async void SeedDatabase(IApplicationBuilder app)
+        {
+            GetAppService<IdentityDataContext>(app).Database.Migrate();
+
+            UserManager<IdentityUser> userManager = GetAppService<UserManager<IdentityUser>>(app);
+
+            RoleManager<IdentityRole> roleManager = GetAppService<RoleManager<IdentityRole>>(app);
+
+            IdentityRole role = await roleManager.FindByNameAsync(adminRole);
+            IdentityUser user = await userManager.FindByNameAsync(adminUser);
+
+            if (role==null)
+            {
+                role = new IdentityRole(adminRole);
+                IdentityResult result = await roleManager.CreateAsync(role);
+                if (!result.Succeeded)
+                {
+                    throw new Exception($"Cannot create role: {result.Errors.FirstOrDefault()}");
+                }
+            }
+
+            if (user==null)
+            {
+                user = new IdentityUser(adminUser);
+                IdentityResult result = await userManager.CreateAsync(user, adminPassword);
+                if (!result.Succeeded)
+                {
+                    throw new Exception($"Cannot create user: {result.Errors.FirstOrDefault()}");
+                }
+            }
+            if (!await userManager.IsInRoleAsync(user, adminRole))
+            {
+                IdentityResult result = await userManager.AddToRoleAsync(user, adminRole);
+                if (!result.Succeeded)
+                {
+                    throw new Exception($"Cannot add user to role {result.Errors.FirstOrDefault()}");
+                }
+            }
+        }
+    }
+}
