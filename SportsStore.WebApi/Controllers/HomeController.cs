c26499e79a435d2b72193eed0fb608cd62673a68
Evgen <evgeniyap88@gmail.com>
2019-08-08 21:11:55 +0500

    page 276, implementing round-trip authentication

---
+++ SportsStore.WebApi/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SportsStore.WebApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Redirect("http://localhost:4200");
+        }
+
+        [Authorize]
+        public string Protected()
+        {
+            return "You've been authenticated";
+        }
+    }
+}
